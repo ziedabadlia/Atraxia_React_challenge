bc3466e9136cb9605a50a5f71dddcf8c685823af
ziedabadlia <80720006+ziedabadlia@users.noreply.github.com>
2023-09-30 08:49:08 +0100

    fixing searchField border

--- src/Components/SearchField/index.style.ts
+++ src/Components/SearchField/index.style.ts
@@ -1,18 +1,19 @@
-
-
 export const searchField = {
-        '& .css-1d3z3hw-MuiOutlinedInput-notchedOutline': {
-          borderColor: '#FFF',
-          borderRadius: '20px !important',
-          border: '1px solid #FFF',
-          height: '60px',
-        },
-        '& .css-aeeyth-MuiInputBase-input-MuiOutlinedInput-input': {
-          color: 'black',
-        },
-        '& .css-18jed1m': {
-          borderRadius: '20px !important',
-        },
-        bgcolor: '#f2f2f2',
-        borderRadius: '20px',
-}
+  '& .css-1d3z3hw-MuiOutlinedInput-notchedOutline': {
+    borderColor: '#FFF',
+    borderRadius: '20px !important',
+    border: '1px solid #FFF',
+    height: '60px',
+  },
+  '& .css-aeeyth-MuiInputBase-input-MuiOutlinedInput-input': {
+    color: 'black',
+  },
+  '& .css-18jed1m': {
+    borderRadius: '20px !important',
+  },
+  '& .MuiOutlinedInput-notchedOutline css-igs3ac': {
+    borderRadius: '20px !important',
+  },
+  bgcolor: '#f2f2f2',
+  borderRadius: '20px',
+};
